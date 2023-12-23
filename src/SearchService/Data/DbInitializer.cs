8563d183495f1227afc3b30cf189ab3e96aa0f59
liam <geffc1454@gmail.com>
2023-12-23 08:54:22 +0800

    [new] adding Search microservices

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,55 @@
+using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Entities;
+using SearchService.Service;
+
+namespace SearchService.Data;
+
+public class DbInitializer {
+
+    public static async Task Initialize(WebApplication app) {
+
+        await DB.InitAsync("SearchDB",
+                MongoClientSettings.FromConnectionString(
+                    app.Configuration["ConnectionStrings:MongoDBConnection"]
+                    )
+                );
+
+        // NOTE: 我們只提供三種查詢的方式
+        await DB.Index<Item>()
+            .Key(i => i.Make, KeyType.Text)
+            .Key(i => i.Model, KeyType.Text)
+            .Key(i => i.Color, KeyType.Text)
+            .CreateAsync();
+
+
+        // var count = await DB.CountAsync<Item>();
+
+        // if (count == 0) {
+        //     var logger = app.Services.GetRequiredService<ILogger<DbInitializer>>();
+        //     logger.LogInformation("No data - will attempt to seed");
+        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        //
+        //     // 這邊的設定是要讓小寫的 json 命名規則，對應到大寫的 C# 命名規則
+        //     var opts = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        //
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, opts);
+        //
+        //     await DB.SaveAsync(items);
+        //
+        // }
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDB();
+
+        Console.WriteLine(items.Count + " return from the auction service");
+
+        if (items.Count > 0) {
+
+            await DB.SaveAsync(items);
+        }
+    }
+
+}
+
---
+++ src/SearchService/Program.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+// if (app.Environment.IsDevelopment()) {}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+/// pooling 的關係程式會一直卡在 polling。原因是
+/// app.Run() 在 await 之後，會等到 DbInitializer 完成
+/// 才會執行。
+/// 解決方法一：使用一個獨立的 Task.Run() 執行
+/// 解決方法二：使用app.Lifetime.ApplicationStarted，
+/// 因為被註冊的 call back 會在 app.Run 執行之後執行，
+/// 因為 lifetime 為 Application`Started` 。
+app.Lifetime.ApplicationStarted.Register(async () => {
+
+    try {
+
+        await DbInitializer.Initialize(app);
+    } catch (Exception ex) {
+        var logger = app.Services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Problem Initializing MongoDB");
+    }
+});
+
+app.Run();
+
+
+// using Polly to make polling policy
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() =>
+    HttpPolicyExtensions.HandleTransientHttpError()
+    .OrResult(res => res.StatusCode == System.Net.HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3)); // 裡面的 Func 給參數的話，會變成 exponential pooling
+
+
