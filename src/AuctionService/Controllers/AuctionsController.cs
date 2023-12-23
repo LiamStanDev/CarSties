2f0edaa85665f57f402288170f4b624766a71875
liam <geffc1454@gmail.com>
2023-12-23 08:53:53 +0800

    [fix] adding search date for dealing with SearchDB request and reformat with automapper

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,16 +20,20 @@ public class AuctionsController : ControllerBase {
     }
 
 
+    /// <summary>
+    /// Return all auctions which update time larger than give date.
+    /// </summary>
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions() {
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date) {
 
+        var query = _context.Auctions.OrderBy(a => a.Item.Make).AsQueryable();
 
-        var auctions = await _context.Auctions.AsQueryable()
-            .Include(a => a.Item)
-            .OrderBy(a => a.Item.Make)
-            .ToListAsync();
+        if (!string.IsNullOrEmpty(date)) {
+            query = query.Where(a => a.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
 
-        return _mapper.Map<List<AuctionDto>>(auctions);
+        // ProjectTo is automapper extension method for not to write include.
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
     [HttpGet("{id}")]
