45e8ce7800a60b85d68a8f0b1d7ea3521fd03fee
liam <geffc1454@gmail.com>
2024-01-10 21:21:39 +0800

    add debug feature in docker container and formatting the code

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,57 +9,56 @@ namespace SearchService.Controllers;
 [Route("api/search")]
 public class SearchController : ControllerBase
 {
-    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
-    {
-        var query = DB.PagedSearch<Item, Item>();
+	public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
+	{
+		var query = DB.PagedSearch<Item, Item>();
 
 
-        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
-        {
-            // the other choice is Search.Fuzzy 
-            query = query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
-        }
+		if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+		{
+			query = query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+		}
 
-        // Sorting
-        query = searchParams.OrderBy switch
-        {
-            "make" => query.Sort(builder => builder.Ascending(i => i.Make)),
-            "new" => query.Sort(builder => builder.Descending(i => i.CreatedAt)),
-            _ => query.Sort(builder => builder.Ascending(i => i.AuctionEnd))
-        };
+		// Sorting
+		query = searchParams.OrderBy switch
+		{
+			"make" => query.Sort(builder => builder.Ascending(i => i.Make)),
+			"new" => query.Sort(builder => builder.Descending(i => i.CreatedAt)),
+			_ => query.Sort(builder => builder.Ascending(i => i.AuctionEnd))
+		};
 
-        // Filtering
-        query = searchParams.FilterBy switch
-        {
-            "finished" => query.Match(i => i.AuctionEnd < DateTime.UtcNow),
-            "endingSoon" => query.Match(i => i.AuctionEnd < DateTime.UtcNow.AddHours(6) && i.AuctionEnd > DateTime.UtcNow), // 六小時候即將結束
-            _ => query.Match(i => i.AuctionEnd > DateTime.UtcNow)
-        };
-
-
-        if (!string.IsNullOrEmpty(searchParams.Seller))
-        {
-            query.Match(i => i.Seller == searchParams.Seller);
-        }
-
-        if (!string.IsNullOrEmpty(searchParams.Winner))
-        {
-            query.Match(i => i.Winner == searchParams.Winner);
-        }
-
-        query.PageNumber(searchParams.PageNumber);
-        query.PageSize(searchParams.PageSize);
+		// Filtering
+		query = searchParams.FilterBy switch
+		{
+			"finished" => query.Match(i => i.AuctionEnd < DateTime.UtcNow),
+			"endingSoon" => query.Match(i => i.AuctionEnd < DateTime.UtcNow.AddHours(6) && i.AuctionEnd > DateTime.UtcNow), // 六小時候即將結束
+			_ => query.Match(i => i.AuctionEnd > DateTime.UtcNow)
+		};
 
 
-        var result = await query.ExecuteAsync();
+		if (!string.IsNullOrEmpty(searchParams.Seller))
+		{
+			query.Match(i => i.Seller == searchParams.Seller);
+		}
+
+		if (!string.IsNullOrEmpty(searchParams.Winner))
+		{
+			query.Match(i => i.Winner == searchParams.Winner);
+		}
+
+		query.PageNumber(searchParams.PageNumber);
+		query.PageSize(searchParams.PageSize);
 
 
-        return Ok(new
-        {
-            results = result.Results,
-            pageCount = result.PageCount,
-            totalCount = result.TotalCount
-        });
-    }
+		var result = await query.ExecuteAsync();
+
+
+		return Ok(new
+		{
+			results = result.Results,
+			pageCount = result.PageCount,
+			totalCount = result.TotalCount
+		});
+	}
 
 }
