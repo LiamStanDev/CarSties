f307a6b46221e874cffec32549eb7814219f4718
liam <geffc1454@gmail.com>
2024-01-12 22:46:01 +0800

    adding authorization test in session page

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,10 +2,29 @@
 
 import { Auction, PagedResult } from "@/types";
 
-export async function getData(query: string): Promise<PagedResult<Auction>> {
+export const getData = async (query: string): Promise<PagedResult<Auction>> => {
   const res = await fetch(`http://localhost:6001/search${query}`);
 
   if (!res.ok) throw new Error("Failed to fetch data");
 
   return res.json();
-}
+};
+
+export const UpdateAuctionTest = async () => {
+  const data = {
+    mileage: Math.floor(Math.random() * 100000) + 1,
+  };
+
+  const res = await fetch(
+    "http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c",
+    {
+      method: "PUT",
+      headers: {},
+      body: JSON.stringify(data),
+    },
+  );
+
+  if (!res.ok) return { status: res.status, message: res.statusText };
+
+  return res.statusText;
+};
--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,5 +1,6 @@
 import { getSession } from "../actions/authAction";
 import Heading from "../components/Heading";
+import AuthTest from "./AuthTest";
 
 const Session = async () => {
   const session = await getSession();
@@ -10,6 +11,9 @@ const Session = async () => {
       <div className="bg-blue-200 border-2 border-blue-500">
         <h3 className="text-lg">Session Data</h3>
         <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+      <div className="mt-4">
+        <AuthTest />
       </div>
     </div>
   );
