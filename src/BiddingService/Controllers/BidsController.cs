48cb2506da88bdfc9bfbfa9405b4cece3da5140c
liam <geffc1454@gmail.com>
2024-01-15 20:36:46 +0800

    add get bids from auction id into controller

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -63,6 +63,18 @@ public class BidsController : ControllerBase
 
 		return Ok(bid);
 	}
+
+
+	[HttpGet("{auctionId}")]
+	public async Task<ActionResult<List<Bid>>> GetBidsForAuction(string auctionId)
+	{
+		var bids = await DB.Find<Bid>()
+			.Match(a => a.AuctionId == auctionId)
+			.Sort(b => b.Descending(a => a.BidTime))
+			.ExecuteAsync();
+
+		return bids;
+	}
 }
 
 
