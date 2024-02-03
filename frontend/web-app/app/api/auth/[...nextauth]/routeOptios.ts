cfc1520309ea17e56d55c2da5a557af757a0ddc1
liam <geffc1454@gmail.com>
2024-02-03 08:06:51 +0800

    update client secret with deployment secret

--- frontend/web-app/app/api/auth/[...nextauth]/routeOptios.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/routeOptios.ts
@@ -10,8 +10,7 @@ export const authOptions: NextAuthOptions = {
       // this need to match Config.cs RedirectUris
       id: "id-server",
       clientId: "nextApp",
-      // clientSecret: process.env.CLIENT_SECRET!,
-      clientSecret: "secret",
+      clientSecret: process.env.CLIENT_SECRET!,
       issuer: process.env.ID_URL,
       authorization: {
         params: {
