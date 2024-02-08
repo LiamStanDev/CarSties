8088d5c2d265e0672d8d1ca2c7e1236631e4accf
liam <geffc1454@gmail.com>
2024-02-08 19:14:51 +0800

    fix useParamsStore syntex error

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
-import { Auction, PagedResult } from "@/types";
 import AppPagination from "../components/AppPagination";
 import { getData } from "../actions/auctionActions";
 import Filters from "./Filters";
